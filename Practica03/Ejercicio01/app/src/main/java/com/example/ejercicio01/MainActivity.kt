457415638220ca86b5aa277664fca7debfe0e3c5
abeluciano <aaragona@ulasalle.edu.pe>
2024-04-13 17:31:17 -0500

    Ejercicio01 terminado

--- Practica03/Ejercicio01/app/src/main/java/com/example/ejercicio01/MainActivity.kt
+++ Practica03/Ejercicio01/app/src/main/java/com/example/ejercicio01/MainActivity.kt
@@ -1,13 +1,23 @@
 package com.example.ejercicio01
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
+
+        val imgLluvia = findViewById<ImageView>(R.id.imgLluvia)
+
+        imgLluvia.setOnClickListener(){
+            var msg = "Mensaje : ${getString(R.string.msg_img)}"
+            Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+        }
     }
 }
