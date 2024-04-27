aa0301e76d123d60a06cbba2460144b8f67c23d8
abeluciano <aaragona@ulasalle.edu.pe>
2024-04-27 00:48:24 -0500

    porque no se conecta el main activity con el activity.xml

--- Practica 05/Ejercicio01/app/src/main/java/com/example/ejericicio01/SelectionFragment.kt
+++ Practica 05/Ejercicio01/app/src/main/java/com/example/ejericicio01/SelectionFragment.kt
@@ -1,59 +1,35 @@
-package com.example.ejericicio01
+package com.example.ejercicio01
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.Spinner
+import androidx.fragment.app.Fragment
+import com.example.ejericicio01.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SelectionFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SelectionFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var spinner: Spinner
+    private lateinit var botonSeleccionar: Button
+    private val nombresImagenes = arrayOf("Lluvia", "Bosque", "Caballo", "Rio", "Monte")
+    private val imagenes = arrayOf(R.drawable.lluvia, R.drawable.bosque, R.drawable.caballo, R.drawable.rio, R.drawable.monte)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_selection, container, false)
+
+        spinner = view.findViewById<Spinner>(R.id.spinner)
+        botonSeleccionar = view.findViewById<Button>(R.id.btn_seleccion)
+
+        val adaptador = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, nombresImagenes)
+        spinner.adapter = adaptador
+
+        botonSeleccionar.setOnClickListener {
+            val posicion = spinner.selectedItemPosition
+            (activity as MainActivity).replaceFragments(imagenes[posicion], nombresImagenes[posicion])
         }
-    }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_selection, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SelectionFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SelectionFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
