a40d6b627201a0c8ec5a98b54cdb6f60236f5796
abeluciano <aaragona@ulasalle.edu.pe>
2024-04-27 00:11:30 -0500

    creados los fragment del ejercicio01

--- Practica 05/Ejercicio01/app/src/main/java/com/example/ejericicio01/MainActivity.kt
+++ Practica 05/Ejercicio01/app/src/main/java/com/example/ejericicio01/MainActivity.kt
@@ -1,20 +1,25 @@
-package com.example.ejericicio01
+package com.example.ejercicio01
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.fragment_container, SelectionFragment())
+                .commit()
         }
+    }
+
+    fun replaceFragments(imagenSeleccionada: Int, nombreImagen: String) {
+        val displayFragment = DisplayFragment.newInstance(imagenSeleccionada, nombreImagen)
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, displayFragment)
+            .addToBackStack(null)
+            .commit()
     }
 }
